87ecae44b26e5f74d1a93e56d8a8f2a695916658
juanpvh <juancm_pvh@hotmail.com>
2019-05-26 15:59:21 -0300

    new

---
+++ installplugin.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+#wget https://manager.servicodigital.info/install-mwc.sh && chmod +x install-mwc.sh && ./install-mwc.sh
+VERDE='\e[0;32m'
+NC='\e[0m' 
+DIR=$(ls -1L /var/www -I22222 -Ihtml) 
+ 
+#Loop para instalar o plugin em todos os diretorios encontrados
+for dominio in ${DIR[@]}; d
+ 	
+	cd /var/www/$dominio/htdocs/wp-content/plugins/
+	wp --allow-root plugin install mainwp-child --activate
+	chown -R www-data:www-data /var/www/$dominio/htdocs/
+	find /var/www/$dominio/htdocs/ -type f -exec chmod 644 {} +
+	find /var/www/$dominio/htdocs/ -type d -exec chmod 755 {} +
+	echo -e "${VERDE}Plugin Instalado em ${dominio} ${NC}"
+	 
+done
+#
+cd ~ 
+rm -rf install-mwc.sh
+
