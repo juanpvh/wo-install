6d0cb348c9e8fc6ee84d1c839b9529c52d89990a
juanpvh <juancm_pvh@hotmail.com>
2020-10-02 09:59:30 -0300

    update

--- install.sh
+++ install.sh
@@ -193,12 +193,12 @@ echo "INSTALANDO WO-CLI.."
     fail2ban-client reload
     fail2ban-client restart
 
-    cp $HOME/wo-install/monit/monitreload.sh /usr/local/bin/monitreload
-    cp $HOME/wo-install/monit/monit2telegram/sendtelegram.sh /usr/local/bin/sendtelegram
+    cp $HOME/wo-install/etc/monit/monitreload.sh /usr/local/bin/monitreload
+    cp $HOME/wo-install/etc/monit/monit2telegram/sendtelegram.sh /usr/local/bin/sendtelegram
     chmod +x /usr/local/bin/sendtelegram
-    cp $HOME/wo-install/monit/monit2telegram/monit2telegram.sh /usr/local/bin/monit2telegram
+    cp $HOME/wo-install/etc/monit/monit2telegram/monit2telegram.sh /usr/local/bin/monit2telegram
     chmod +x /usr/local/bin/monit2telegram
-    cp  $HOME/wo-install/monit/monit2telegram/telegramrc /etc/
+    cp  $HOME/wo-install/etc/monit/monit2telegram/telegramrc /etc/
 
 (crontab -l; echo "0 0 * * * bash /usr/local/bin/monitreload >> /var/log/monitreload.log /dev/null 2>&1") | crontab -
 
